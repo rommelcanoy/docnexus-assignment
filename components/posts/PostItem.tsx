6310156eaada5ea698369fb0fb8b9a2cf11df06b
Rommel Canoy <48854015+rommelcanoy@users.noreply.github.com>
2023-08-31 20:54:25 +0800

    feat: add delete post functionality

--- components/posts/PostItem.tsx
+++ components/posts/PostItem.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import { useCallback, useMemo } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { AiFillHeart, AiOutlineHeart, AiOutlineMessage } from 'react-icons/ai';
 import { formatDistanceToNowStrict } from 'date-fns';
 
@@ -8,6 +8,12 @@ import useCurrentUser from '@/hooks/useCurrentUser';
 import useLike from '@/hooks/useLike';
 
 import Avatar from '../Avatar';
+import { toast } from 'react-hot-toast';
+// import useUser from '@/hooks/useUser';
+import axios from 'axios';
+import usePost from '@/hooks/usePost';
+import usePosts from '@/hooks/usePosts';
+import { FaTrashAlt } from 'react-icons/fa';
 interface PostItemProps {
   data: Record<string, any>;
   userId?: string;
@@ -16,9 +22,16 @@ interface PostItemProps {
 const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
   const router = useRouter();
   const loginModal = useLoginModal();
-
   const { data: currentUser } = useCurrentUser();
+  // const { mutate: mutateFetchedUser } = useUser(currentUser?.id);
+  const { mutate: mutateFetchedPost } = usePost(data.id);
+  const { mutate: mutateFetchedPosts } = usePosts(userId);
   const { hasLiked, toggleLike } = useLike({ postId: data.id, userId });
+  const [isLoading, setIsLoading] = useState(false);
+  // const [deleteId, setDeleteId] = useState('');
+
+  // console.log('currentUser id', currentUser.id);
+  // console.log('data id', data.userId);
 
   const goToUser = useCallback((ev: any) => {
     ev.stopPropagation();
@@ -49,10 +62,42 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
     return formatDistanceToNowStrict(new Date(data.createdAt));
   }, [data.createdAt])
 
+  const onDelete = useCallback(async (postId: string) => {
+
+    const confirm = window.confirm("Are you sure you want to delete this post?");
+
+    if (!confirm) {
+      return;
+    }
+
+    try {
+      setIsLoading(true);
+
+      await axios.delete(`/api/posts/${postId}`);
+      mutateFetchedPost();
+      mutateFetchedPosts();
+
+      toast.success('Deleted post successfully');
+    } catch (error) {
+      toast.error('Something went wrong');
+    } finally {
+      setIsLoading(false);
+    }
+  }, [mutateFetchedPost, mutateFetchedPosts]);
+
   return (
-    <div
-      onClick={goToPost}
-      className="
+    <div className='relative'>
+      {
+        currentUser?.id === data.userId ?
+          <button className={`absolute right-5 top-5 bg-blue-500 hover:bg-opacity-80 p-2 rounded text-white disabled:bg-gray-300 disabled:text-gray-500`}
+            disabled={isLoading} onClick={() => onDelete(data.id)}>
+            <FaTrashAlt />
+          </button> : null
+      }
+
+      <div
+        onClick={goToPost}
+        className="
         p-5 
         cursor-pointer 
         hover:bg-gray-200
@@ -60,42 +105,48 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
         bg-white
         rounded-xl
         overflow-hidden
+        border
       ">
-      <div className="flex flex-row items-start gap-3">
-        <Avatar userId={data.user.id} />
-        <div>
-          <div className="flex flex-row items-center gap-2">
-            <p
-              onClick={goToUser}
-              className="
+        <div className="flex flex-row items-start gap-3">
+          <Avatar userId={data.user.id} />
+          <div className='w-full'>
+            <div className="flex flex-row items-center gap-2">
+              <div className='flex flex-col w-full'>
+                <p
+                  onClick={goToUser}
+                  className="
                 text-black 
                 font-semibold 
                 cursor-pointer 
                 hover:underline
+                text-sm
             ">
-              {data.user.name}
-            </p>
-            <span
-              onClick={goToUser}
-              className="
+                  {data.user.name}
+                </p>
+                <span
+                  onClick={goToUser}
+                  className="
                 text-neutral-500
                 cursor-pointer
                 hover:underline
                 hidden
                 md:block
+                text-xs
             ">
-              @{data.user.username}
-            </span>
-            <span className="text-neutral-500 text-sm">
-              {createdAt}
-            </span>
-          </div>
-          <div className="text-black mt-1">
-            {data.body}
-          </div>
-          <div className="flex flex-row items-center mt-3 gap-10">
-            <div
-              className="
+                  {data.user.title}
+                </span>
+                <span className="text-neutral-500 text-xs">
+                  {createdAt}
+                </span>
+              </div>
+
+            </div>
+            <div className="text-black mt-1">
+              {data.body}
+            </div>
+            <div className="flex flex-row items-center mt-3 gap-10">
+              <div
+                className="
                 flex 
                 flex-row 
                 items-center 
@@ -105,14 +156,14 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
                 transition 
                 hover:text-blue-500
             ">
-              <AiOutlineMessage size={20} />
-              <p>
-                {data.comments?.length || 0}
-              </p>
-            </div>
-            <div
-              onClick={onLike}
-              className="
+                <AiOutlineMessage size={20} />
+                <p>
+                  {data.comments?.length || 0}
+                </p>
+              </div>
+              <div
+                onClick={onLike}
+                className="
                 flex 
                 flex-row 
                 items-center 
@@ -122,10 +173,11 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
                 transition 
                 hover:text-red-500
             ">
-              <LikeIcon color={hasLiked ? 'red' : ''} size={20} />
-              <p>
-                {data.likedIds.length}
-              </p>
+                <LikeIcon color={hasLiked ? 'red' : ''} size={20} />
+                <p>
+                  {data.likedIds.length}
+                </p>
+              </div>
             </div>
           </div>
         </div>
--- pages/api/posts/[postId].ts
+++ pages/api/posts/[postId].ts
@@ -3,37 +3,58 @@ import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@/libs/prismadb";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'GET') {
+  if (req.method === 'GET') {
+    try {
+      const { postId } = req.query;
+
+      if (!postId || typeof postId !== 'string') {
+        throw new Error('Invalid ID');
+      }
+
+      const post = await prisma.post.findUnique({
+        where: {
+          id: postId,
+        },
+        include: {
+          user: true,
+          comments: {
+            include: {
+              user: true
+            },
+            orderBy: {
+              createdAt: 'desc'
+            }
+          },
+        },
+      });
+
+      return res.status(200).json(post);
+    } catch (error) {
+      console.log(error);
+      return res.status(400).end();
+    }
+  } else if (req.method === 'DELETE') {
+    try {
+      const { postId } = req.query;
+
+      if (!postId || typeof postId !== 'string') {
+        throw new Error('Invalid ID');
+      }
+
+      const deletedPost = await prisma.post.delete({
+        where: {
+          id: postId,
+        },
+      });
+
+      return res.status(200).json({ message: "Post deleted successfully" });
+
+    } catch (error) {
+      console.log(error);
+      return res.status(500).end();
+    }
+  } else {
     return res.status(405).end();
   }
 
-  try {
-    const { postId } = req.query;
-
-    if (!postId || typeof postId !== 'string') {
-      throw new Error('Invalid ID');
-    }
-
-    const post = await prisma.post.findUnique({
-      where: {
-        id: postId,
-      },
-      include: {
-        user: true,
-        comments: {
-          include: {
-            user: true
-          },
-          orderBy: {
-            createdAt: 'desc'
-          }
-        },
-      },
-    });
-
-    return res.status(200).json(post);
-  } catch (error) {
-    console.log(error);
-    return res.status(400).end();
-  }
 }
