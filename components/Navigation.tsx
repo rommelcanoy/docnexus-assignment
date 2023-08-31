e7f3a15c56f96930528b39e7eb382f151495a7fa
Rommel Canoy <48854015+rommelcanoy@users.noreply.github.com>
2023-08-31 21:33:55 +0800

    style: replace the logo of the platform with docnexus logo

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,15 +1,14 @@
-
+import docnexusLogo from '@/public/docnexus.svg';
+import Image from 'next/image';
+import Link from 'next/link';
 interface NavigationProps {
 }
 
 const Navigation: React.FC<NavigationProps> = () => {
   return (
-    <div className='bg-white py-2 px-5 fixed top-0 left-0 right-0 z-30 border-b'>
-      <div className="mx-auto xl:px-30 max-w-5xl flex justify-between">
-        <div>DocNexus</div>
-        <div>
-
-        </div>
+    <div className='bg-white py-2 px-5 fixed top-0 left-0 right-0 z-30 shadow-sm border-b'>
+      <div className="mx-auto xl:px-30 max-w-5xl flex justify-center">
+        <Link href="/"><Image src={docnexusLogo} alt="DocNexus" className='w-24 h-auto rounded-md' /></Link>
       </div>
     </div>
   )
