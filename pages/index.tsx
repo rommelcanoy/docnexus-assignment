b3709435da597695c16709d1c9ff1fbb4ee8bed4
Rommel Canoy <48854015+rommelcanoy@users.noreply.github.com>
2023-09-01 05:55:46 +0800

    chore: update the visibility of following/global when user is not logged in

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,12 +23,14 @@ export default function Home() {
       {/* <Header label="Home" /> */}
       <Form placeholder="What do you want to talk about?" />
       <div className="flex justify-center mb-4">
-        <div className="flex gap-2 mx-auto py-1 px-2 rounded bg-white">
-          {
-            currentUser ? <button className={`${following ? 'bg-gray-200' : null} py-1 px-2 rounded`} onClick={() => setFollowing(true)}>Following</button> : null
-          }
-          <button className={`${!following ? 'bg-gray-200' : null} py-1 px-2 rounded`} onClick={() => setFollowing(false)}>Global</button>
-        </div>
+        {
+          currentUser ?
+            <div className="flex gap-2 mx-auto py-1 px-2 rounded bg-white">
+              <button className={`${following ? 'bg-gray-200' : null} py-1 px-2 rounded`} onClick={() => setFollowing(true)}>Following</button>
+              <button className={`${!following ? 'bg-gray-200' : null} py-1 px-2 rounded`} onClick={() => setFollowing(false)}>Global</button>
+            </div>
+            : null
+        }
       </div>
       <div>
         {
